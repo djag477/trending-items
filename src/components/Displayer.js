b364c2c9a74f7fc878ec20e05886b681f388454e
djag477 <101579022+djag477@users.noreply.github.com>
2022-07-31 21:01:49 +0200

    add comments

--- src/components/Displayer.js
+++ src/components/Displayer.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import {CardGroup} from 'reactstrap'
-import TrenderCard from './TrenderCard'
+import React from "react";
+import { CardGroup } from "reactstrap";
+import TrenderCard from "./TrenderCard";
 
-const Displayer = ({trenders}) => {
+const Displayer = ({ trenders }) => {
   return (
-    <CardGroup data-testid="test-2">{trenders.map((elem, i)=>{
-        return <TrenderCard key={i} elem={elem}></TrenderCard>
-    })}</CardGroup>
-  )
-}
+    <CardGroup data-testid="test-2">
+      {trenders.map((elem, i) => {
+        /* Maps through the final set of data to render the card elements and passes props to build them */
+        return <TrenderCard key={i} elem={elem}></TrenderCard>;
+      })}
+    </CardGroup>
+  );
+};
 
-export default Displayer
+export default Displayer;
